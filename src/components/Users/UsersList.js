1840f1718799295da9a65b7109f3798a7705cb11
Roberto <epictetusrl@yahoo.com>
2019-02-15 18:27:40 +0100

    localStorage session

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,24 +1,65 @@
 import React from 'react';
 
-const UsersList = ({ users }) => (
-	<ul>
-		{users.map((user) => (
-			<li key={user.id}>
-				<span>
-					<strong>ID</strong>
-				</span>
-				{user.id}
-				<span>
-					<strong>E-Mail</strong>
-				</span>
-				{user.email}
-				<span>
-					<strong>Username</strong>
-				</span>
-				{user.username}
-			</li>
-		))}
-	</ul>
-);
+class UserListBase extends Component {
+	constructor(props) {
+		super(props);
 
-export default UsersList;
+		this.state = {
+			loading: false,
+			users: []
+		};
+	}
+
+	componentDidMount() {
+		this.setState({ loading: true });
+
+		this.props.firebase.users().on('value', (snapshot) => {
+			const usersObject = snapshot.val();
+
+			const usersList = Object.keys(usersObject).map((key) => ({
+				...usersObject[key],
+				uid: key
+			}));
+
+			this.setState({
+				users: usersList,
+				loading: false
+			});
+		});
+	}
+
+	componentWillUnmount() {
+		this.props.firebase.users().off();
+	}
+
+	render() {
+		const { users, loading } = this.state;
+
+		return (
+			<div>
+				<h2>Users</h2>
+				{loading && <div>Loading ...</div>}
+				<ul>
+					{users.map((user) => (
+						<li key={user.uid}>
+							<span>
+								<strong>ID:</strong> {user.uid}
+							</span>
+							<span>
+								<strong>E-Mail:</strong> {user.email}
+							</span>
+							<span>
+								<strong>Username:</strong> {user.username}
+							</span>
+							<span>
+								<Link to={`${ROUTES.ADMIN}/${user.uid}`}>Details</Link>
+							</span>
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default UsersListBase;
